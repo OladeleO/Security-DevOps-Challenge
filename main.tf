295f0375dbe323bef883a074cbfbb00e318ea414
OladeleO <58373625+OladeleO@users.noreply.github.com>
2021-12-04 19:39:09 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,12 +7,27 @@ provider "google" {
    zone    = var.region
 }
 
-resource "google_container_cluster" "my_vpc_native_cluster" {
-   name                 = var.gke_cluster_name
-   location             = var.zone
-   initial_node_count   = 1
+#resource "google_container_cluster" "my_vpc_native_cluster" {
+#   name                 = var.gke_cluster_name
+#   location             = var.zone
+#   initial_node_count   = 1
    
-   network              = "default"
-   subnetwork           = "default"
+#   network              = "default"
+#   subnetwork           = "default"
   
+#}
+
+resource "google_compute_network" "vpc_network" {
+  project                 = "my-project-name"
+  name                    = "vpc-server"
+  auto_create_subnetworks = true
+  mtu                     = 1460
 }
+
+resource "google_compute_network" "vpc_network" {
+  project                 = "my-project-name"
+  name                    = "vpc-client"
+  auto_create_subnetworks = true
+  mtu                     = 1460
+}
+
