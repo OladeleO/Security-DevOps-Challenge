f8865c611c50751a2641a3f4f021274243e8cf57
OladeleO <58373625+OladeleO@users.noreply.github.com>
2021-12-05 20:52:58 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -252,6 +252,6 @@ resource "google_compute_route" "route_server_to_client" {
 }
 
 output "server_public_ip" {
-  value = google_compute_instance.vm_2.external_ip
+  value = google_compute_instance.vm_2.network_interface.access_config.nat_ip
 }
  ##################################################################################
